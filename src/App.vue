1d61792551d59c37a450a667c9cf0a8c16ab3542
anthony ervin <ajervin1@asu.edu>
2020-09-14 08:35:14 -0700

    added firebase

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
 	<main id="app" class="">
-		
+
 		<navbar></navbar>
-		
+
 		<router-view></router-view>
 	</main>
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
-	import ItemList from "./components/ItemList.vue";
+
 	import Navbar from "./components/Navbar.vue";
-	
-	
+
+
 	export default Vue.extend( {
 		name: 'App',
 		components: { Navbar },
